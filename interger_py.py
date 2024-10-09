6dcbbc56141c7d0141a3e93bb81c467860996425
Applemazer <anselchengzean@gmail.com>
2024-10-09 13:19:13 +0800

    Add files via upload

---
+++ interger_py.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+list = [1,2,3,4,5,6,7,8,9,10]
+print(list)
+
+for i in range (1,11):
+    print(i)
+
+for o in range (5,16):
+    print (o)
+
+# for range in python integer starts from 0 but r starts at 1
+
+for t in range (5):
+    print (t)
